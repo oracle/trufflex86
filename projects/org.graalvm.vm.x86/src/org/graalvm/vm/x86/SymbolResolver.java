6bcbc62c75e1e5ffa7e976e4797fdc5603d8414e
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-03-02 14:26:53 +0100

    Prefer global symbols and use local symbols as fallback

--- projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/SymbolResolver.java
+++ projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/SymbolResolver.java
@@ -2,17 +2,37 @@ package org.graalvm.vm.x86;
 
 import java.util.Map.Entry;
 import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import com.everyware.posix.elf.Symbol;
 
 public class SymbolResolver {
     private final NavigableMap<Long, Symbol> symbols;
+    private final NavigableMap<Long, Symbol> globalSymbols;
 
     public SymbolResolver(NavigableMap<Long, Symbol> symbols) {
         this.symbols = symbols;
+
+        // compute global symbols
+        this.globalSymbols = new TreeMap<>();
+        for (Entry<Long, Symbol> entry : symbols.entrySet()) {
+            Symbol sym = entry.getValue();
+            if (sym.getBind() == Symbol.GLOBAL) {
+                globalSymbols.put(sym.getValue(), sym);
+            }
+        }
     }
 
     public Symbol getSymbol(long pc) {
+        Symbol global = getSymbol(pc, globalSymbols);
+        if (global == null) {
+            return getSymbol(pc, symbols);
+        } else {
+            return global;
+        }
+    }
+
+    private static Symbol getSymbol(long pc, NavigableMap<Long, Symbol> symbols) {
         Symbol sym = symbols.get(pc);
         if (sym != null) {
             return sym;
@@ -27,5 +47,9 @@ public class SymbolResolver {
             }
         }
         return null;
+    }
+
+    public Symbol getSymbolExact(long pc) {
+        return symbols.get(pc);
     }
 }
