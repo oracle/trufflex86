e3313f2cd36f4ce76fd0b484fe9890b70169a318
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-03-16 10:11:47 +0100

    Increase stack size to 8MB (default on Linux/x86_64)

--- projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/AMD64.java
+++ projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/AMD64.java
@@ -1,7 +1,7 @@
 package org.graalvm.vm.x86;
 
 public class AMD64 {
-    public static final long STACK_SIZE = 1024 * 1024; // 1M
+    public static final long STACK_SIZE = 8 * 1024 * 1024; // 8M
     public static final long STACK_ADDRESS = 0x7fff6c845000L;
     // public static final long STACK_ADDRESS = 0xf6fff000L;
     public static final long STACK_BASE = STACK_ADDRESS - STACK_SIZE;
