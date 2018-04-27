4d5dc876f9aed0adc935ad2894e581b327e2f335
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-04-27 15:01:02 +0200

    Use new trace compilation system

--- projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/node/InterpreterRootNode.java
+++ projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/node/InterpreterRootNode.java
@@ -1,18 +1,18 @@
 package org.graalvm.vm.x86.node;
 
 import org.graalvm.vm.x86.ArchitecturalState;
-import org.graalvm.vm.x86.node.flow.DispatchNode;
+import org.graalvm.vm.x86.node.flow.InterTraceDispatchNode;
 import org.graalvm.vm.x86.node.init.InitializerNode;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 public class InterpreterRootNode extends AMD64Node {
     @Child private InitializerNode initializer;
-    @Child private DispatchNode interpreter;
+    @Child private InterTraceDispatchNode interpreter;
 
     public InterpreterRootNode(ArchitecturalState state, String programName) {
         initializer = new InitializerNode(state, programName);
-        interpreter = new DispatchNode(state);
+        interpreter = new InterTraceDispatchNode(state);
     }
 
     public Object execute(VirtualFrame frame) {
