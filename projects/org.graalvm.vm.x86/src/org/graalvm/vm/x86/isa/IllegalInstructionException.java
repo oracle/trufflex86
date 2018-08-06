12c4cd75f5172b5456915432c67f6513d63f02e7
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-08-06 16:17:01 +0200

    Improve error message for IllegalInstructionException

--- projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/isa/IllegalInstructionException.java
+++ projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/isa/IllegalInstructionException.java
@@ -1,5 +1,7 @@
 package org.graalvm.vm.x86.isa;
 
+import org.graalvm.vm.memory.util.HexFormatter;
+
 public class IllegalInstructionException extends RuntimeException {
     private static final long serialVersionUID = -2754913616070473400L;
 
@@ -7,7 +9,7 @@ public class IllegalInstructionException extends RuntimeException {
     private final byte[] instruction;
 
     public IllegalInstructionException(long pc, byte[] instruction, String message) {
-        super(message);
+        super("[pc=" + HexFormatter.tohex(pc, 16) + ": " + getBinary(instruction) + "] " + message);
         this.pc = pc;
         this.instruction = instruction;
     }
@@ -18,5 +20,16 @@ public class IllegalInstructionException extends RuntimeException {
 
     public byte[] getInstruction() {
         return instruction;
+    }
+
+    private static String getBinary(byte[] b) {
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < b.length; i++) {
+            if (i > 0) {
+                buf.append(' ');
+            }
+            buf.append(HexFormatter.tohex(Byte.toUnsignedInt(b[i]), 2));
+        }
+        return buf.toString();
     }
 }
