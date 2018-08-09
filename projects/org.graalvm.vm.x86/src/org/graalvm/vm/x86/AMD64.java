9c159a0e81f280769ccb6ba6fbceb0e3e090e125
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-08-09 11:44:54 +0200

    Move stack to native memory

--- projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/AMD64.java
+++ projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/AMD64.java
@@ -2,9 +2,11 @@ package org.graalvm.vm.x86;
 
 public class AMD64 {
     public static final long STACK_SIZE = 8 * 1024 * 1024; // 8M
-    // public static final long STACK_ADDRESS = 0x7fff6c845000L;
+    public static final long STACK_ADDRESS_DBG = 0x7fff6c845000L;
+    public static final long STACK_ADDRESS_NATIVE = 0x1ffffffff000L;
     // public static final long STACK_ADDRESS = 0xf6fff000L;
-    public static final long STACK_ADDRESS = 0x0000800000000000L;
+    // public static final long STACK_ADDRESS = 0x0000800000000000L;
+    public static final long STACK_ADDRESS = Options.getBoolean(Options.DEBUG_STATIC_ENV) ? STACK_ADDRESS_DBG : STACK_ADDRESS_NATIVE;
     public static final long STACK_BASE = STACK_ADDRESS - STACK_SIZE;
 
     public static final int DCACHE_LINE_SIZE = 0x20;
