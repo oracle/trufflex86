1661172de40d2f9089f0161fc9efef7090569726
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-09-20 11:28:58 +0200

    Add SHR test

--- projects/org.graalvm.vm.x86.test/src/org/graalvm/vm/x86/isa/test/ShrTest.java
+++ projects/org.graalvm.vm.x86.test/src/org/graalvm/vm/x86/isa/test/ShrTest.java
@@ -2,6 +2,7 @@ package org.graalvm.vm.x86.isa.test;
 
 import org.graalvm.vm.x86.isa.instruction.Shr.Shrb;
 import org.graalvm.vm.x86.test.InstructionTest;
+import org.graalvm.vm.x86.test.runner.TestRunner;
 import org.junit.Test;
 
 public class ShrTest extends InstructionTest {
@@ -11,5 +12,58 @@ public class ShrTest extends InstructionTest {
     @Test
     public void test1() {
         check(MACHINECODE1, ASSEMBLY1, Shrb.class);
+    }
+
+    @Test
+    public void shr() throws Exception {
+        String stdout = "0000:00:0000:0000\n" +
+                        "0000:05:0000:0044\n" +
+                        "1000:00:1000:0000\n" +
+                        "1000:05:0080:0000\n" +
+                        "ffff:05:07ff:0805\n" +
+                        "8000:08:0080:0800\n" +
+                        "8001:0f:0001:0800\n" +
+                        "8001:10:0000:0845\n" +
+                        "ffff:0f:0001:0801\n" +
+                        "ffff:10:0000:0845\n" +
+                        "8000:01:4000:0804\n" +
+                        "c000:01:6000:0804\n" +
+                        "c000:02:3000:0804\n" +
+                        "c000:02:3000:0804\n" +
+                        "c000:04:0c00:0804\n" +
+                        "c000:00:c000:0000\n" +
+                        "c0de:00:c0de:0000\n" +
+                        "efde:08:00ef:0801\n" +
+                        "efde:04:0efd:0801\n" +
+                        "0000:01:0000:0044\n" +
+                        "1000:01:0800:0004\n" +
+                        "0000:02:0000:0044\n" +
+                        "1000:02:0400:0004\n" +
+                        "c0de:10:0000:0845\n" +
+                        "c0de:18:0000:0845\n" + // undefined: (i7: 0844)
+                        "c0de:20:c0de:0000\n" +
+                        "c0de:30:0000:0845\n" +
+                        "c0de:38:0000:0845\n" + // undefined: (i7: 0844)
+                        "c0de:40:c0de:0000\n" +
+                        "0001:00:0001:0000\n" +
+                        "0001:05:0000:0044\n" +
+                        "0001:08:0000:0044\n" +
+                        "0001:01:0000:0045\n" +
+                        "0003:01:0001:0001\n" +
+                        "0003:02:0000:0045\n" +
+                        "0003:02:0000:0045\n" +
+                        "0003:04:0000:0044\n" +
+                        "0003:00:0003:0000\n" +
+                        "0000:01:0000:0044\n" +
+                        "0001:01:0000:0045\n" +
+                        "0000:02:0000:0044\n" +
+                        "0001:02:0000:0044\n" +
+                        "dffffdea:1:6ffffef5:0804\n" +
+                        "0ffffdea:1:07fffef5:0004\n" +
+                        "0ffffde0:1:07fffef0:0004\n" +
+                        "0ffffde1:1:07fffef0:0005\n" +
+                        "0000000000000163:20:0000000000000000:0044\n" +
+                        "1630000000000000:20:0000000016300000:0004\n";
+        TestRunner.run("shr.elf", new String[0], "", stdout, "", 0);
     }
 }
---
+++ tests/org.graalvm.vm.x86.testcases.c/src/shr.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+#include <stdio.h>
+#include "flags.h"
+
+void test_shrw(unsigned short value, int count)
+{
+	int out_flags;
+	unsigned short out;
+	__asm__ volatile("pushf\n\t"
+			 "xorq	%%rax, %%rax\n\t"
+			 "push	%%rax\n\t"
+			 "popf\n\t"
+			 "shrw	%%cl, %[value]\n\t"
+			 "pushf\n\t"
+			 "pop	%%rax\n\t"
+			 "popf\n\t"
+			 :         "=a"(out_flags),
+			   [value] "=r"(out)
+			 :         "1"(value),
+			           "c"(count));
+	printf("%04hx:%02x:%04hx:%04x\n", value, count, out, (out_flags & CC_MASK));
+}
+
+void test_shrl_1(unsigned int value)
+{
+	int out_flags;
+	unsigned int out;
+	__asm__ volatile("pushf\n\t"
+			 "xorq	%%rax, %%rax\n\t"
+			 "push	%%rax\n\t"
+			 "popf\n\t"
+			 "shrl	$1, %[value]\n\t"
+			 "pushf\n\t"
+			 "pop	%%rax\n\t"
+			 "popf\n\t"
+			 :         "=a"(out_flags),
+			   [value] "=r"(out)
+			 :         "1"(value));
+	printf("%08x:1:%08x:%04x\n", value, out, (out_flags & CC_MASK));
+}
+
+void test_shrq_32(unsigned long value)
+{
+	int out_flags;
+	unsigned long out;
+	__asm__ volatile("pushf\n\t"
+			 "xorq	%%rax, %%rax\n\t"
+			 "push	%%rax\n\t"
+			 "popf\n\t"
+			 "shrq	$0x20, %[value]\n\t"
+			 "pushf\n\t"
+			 "pop	%%rax\n\t"
+			 "popf\n\t"
+			 :         "=a"(out_flags),
+			   [value] "=r"(out)
+			 :         "1"(value));
+	printf("%016lx:20:%016lx:%04x\n", value, out, (out_flags & CC_MASK));
+}
+
+int main(void)
+{
+	test_shrw(0x0000, 0);
+	test_shrw(0x0000, 5);
+	test_shrw(0x1000, 0);
+	test_shrw(0x1000, 5);
+	test_shrw(0xFFFF, 5);
+	test_shrw(0x8000, 8);
+	test_shrw(0x8001, 15);
+	test_shrw(0x8001, 16);
+	test_shrw(0xFFFF, 15);
+	test_shrw(0xFFFF, 16);
+	test_shrw(0x8000, 1);
+	test_shrw(0xC000, 1);
+	test_shrw(0xC000, 2);
+	test_shrw(0xC000, 2);
+	test_shrw(0xC000, 4);
+	test_shrw(0xC000, 0);
+	test_shrw(0xC0DE, 0);
+	test_shrw(0xEFDE, 8);
+	test_shrw(0xEFDE, 4);
+	test_shrw(0x0000, 1);
+	test_shrw(0x1000, 1);
+	test_shrw(0x0000, 2);
+	test_shrw(0x1000, 2);
+	test_shrw(0xC0DE, 16);
+	test_shrw(0xC0DE, 24);
+	test_shrw(0xC0DE, 32);
+	test_shrw(0xC0DE, 48);
+	test_shrw(0xC0DE, 56);
+	test_shrw(0xC0DE, 64);
+	test_shrw(0x0001, 0);
+	test_shrw(0x0001, 5);
+	test_shrw(0x0001, 8);
+	test_shrw(0x0001, 1);
+	test_shrw(0x0003, 1);
+	test_shrw(0x0003, 2);
+	test_shrw(0x0003, 2);
+	test_shrw(0x0003, 4);
+	test_shrw(0x0003, 0);
+	test_shrw(0x0000, 1);
+	test_shrw(0x0001, 1);
+	test_shrw(0x0000, 2);
+	test_shrw(0x0001, 2);
+
+	test_shrl_1(0xdffffdea);
+	test_shrl_1(0x0ffffdea);
+	test_shrl_1(0x0ffffde0);
+	test_shrl_1(0x0ffffde1);
+
+	test_shrq_32(0x0000000000000163L);
+	test_shrq_32(0x1630000000000000L);
+	return 0;
+}
