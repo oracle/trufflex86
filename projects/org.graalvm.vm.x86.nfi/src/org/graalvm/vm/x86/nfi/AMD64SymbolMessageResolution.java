6307f8dd0cf69d354eaf5356ccf56d727a797b54
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-11-21 11:42:39 +0100

    Add missing TruffleBoundary

--- projects/org.graalvm.vm.x86.nfi/src/org/graalvm/vm/x86/nfi/AMD64SymbolMessageResolution.java
+++ projects/org.graalvm.vm.x86.nfi/src/org/graalvm/vm/x86/nfi/AMD64SymbolMessageResolution.java
@@ -4,25 +4,20 @@ import org.graalvm.vm.x86.nfi.TypeConversion.AsStringNode;
 import org.graalvm.vm.x86.nfi.TypeConversionFactory.AsStringNodeGen;
 
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.interop.CanResolve;
 import com.oracle.truffle.api.interop.MessageResolution;
 import com.oracle.truffle.api.interop.Resolve;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.nfi.types.NativeSignature;
-import com.oracle.truffle.nfi.types.NativeSimpleTypeMirror;
-import com.oracle.truffle.nfi.types.NativeTypeMirror;
 import com.oracle.truffle.nfi.types.Parser;
 
 @MessageResolution(receiverType = AMD64Symbol.class)
 public class AMD64SymbolMessageResolution {
-    protected static String str(NativeTypeMirror type) {
-        switch (type.getKind()) {
-            case SIMPLE:
-                return ((NativeSimpleTypeMirror) type).getSimpleType().toString();
-            default:
-                return type.getKind().toString();
-        }
+    @TruffleBoundary
+    private static NativeSignature parseSignature(String signature) {
+        return Parser.parseSignature(signature);
     }
 
     @Resolve(message = "INVOKE")
@@ -32,7 +27,7 @@ public class AMD64SymbolMessageResolution {
         public TruffleObject access(AMD64Symbol receiver, String name, Object[] args) {
             if (name.equals("bind")) {
                 String signature = asString.execute(args[0]);
-                NativeSignature parsed = Parser.parseSignature(signature);
+                NativeSignature parsed = parseSignature(signature);
                 return new AMD64Function(receiver.getName(), receiver.getAddress(), parsed);
             } else {
                 CompilerDirectives.transferToInterpreter();
