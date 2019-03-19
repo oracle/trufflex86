48308640c69fc1a3ff81279fc50013a32cfeac36
Daniel Pekarek <pekarekdaniel@gmail.com>
2019-03-19 17:49:38 +0100

    Simplify initialization in InterpreterStartNode

--- projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/node/InterpreterStartNode.java
+++ projects/org.graalvm.vm.x86/src/org/graalvm/vm/x86/node/InterpreterStartNode.java
@@ -41,30 +41,23 @@
 package org.graalvm.vm.x86.node;
 
 import org.graalvm.vm.x86.AMD64Context;
-import org.graalvm.vm.x86.AMD64Language;
 import org.graalvm.vm.x86.ArchitecturalState;
 
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 public class InterpreterStartNode extends AMD64RootNode {
-    private final String programName;
-
     @Child private InterpreterRootNode interpreter;
 
     public InterpreterStartNode(TruffleLanguage<AMD64Context> language, FrameDescriptor fd, String programName) {
         super(language, fd);
-        this.programName = programName;
+        ArchitecturalState state = language.getContextReference().get().getState();
+        interpreter = insert(new InterpreterRootNode(state, programName));
     }
 
     @Override
     public Object execute(VirtualFrame frame) {
-        CompilerDirectives.transferToInterpreterAndInvalidate();
-        TruffleLanguage<AMD64Context> language = getLanguage(AMD64Language.class);
-        ArchitecturalState state = language.getContextReference().get().getState();
-        interpreter = insert(new InterpreterRootNode(state, programName));
         return interpreter.execute(frame);
     }
 
